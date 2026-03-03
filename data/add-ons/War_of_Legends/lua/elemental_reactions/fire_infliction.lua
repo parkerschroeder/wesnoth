06a4ffd29ab699da91829296fdfafbc224abc13b
parkerschroeder <schroederp21@gmail.com>
2026-03-02 20:04:40 -0800

    copy over wol addon

---
+++ data/add-ons/War_of_Legends/lua/elemental_reactions/fire_infliction.lua
@@ -0,0 +1,17 @@
+local T = wml.tag
+
+function wesnoth.wml_actions.WOL_add_fire_status(cfg)	
+	local units = wesnoth.units.find_on_map(cfg)
+	for _,u in pairs(units) do
+		u:add_modification("object",{
+			id = "WOL_fire_status_object",
+            duration = "turn",
+            -- add overlay
+			T.effect{
+				apply_to="overlay",
+				add="misc/fire-overlay.png"
+			}
+		})
+		u.status.fire_status = true
+	end
+end
